da6855e6da842dac647c7b0130f70f683f08bc47
bakansm <khuynhbao5@gmail.com>
2025-07-28 15:26:15 +0700

    revert: inngest client

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,13 +1,13 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-// export const inngest = new Inngest({ id: "sagent" });
+export const inngest = new Inngest({ id: "sagent" });
 
-export const inngest = new Inngest({
-  id: "sagent",
-  baseUrl:
-    process.env.NODE_ENV === "production"
-      ? "https://api.inngest.com"
-      : "http://127.0.0.1:8288",
-  isDev: process.env.NODE_ENV === "development",
-});
+// export const inngest = new Inngest({
+//   id: "sagent",
+//   baseUrl:
+//     process.env.NODE_ENV === "production"
+//       ? "https://api.inngest.com"
+//       : "http://127.0.0.1:8288",
+//   isDev: process.env.NODE_ENV === "development",
+// });
